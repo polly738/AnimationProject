d9a2e039d6ba7d50dd08726bd5a4357a8b0a895c
gbigas <94164838+gbigas@users.noreply.github.com>
2022-04-07 07:44:59 -0400

    Add files via upload

---
+++ src/RRect.java
@@ -0,0 +1,53 @@
+/**
+ *Extends Line class of javafx to allow for Line class to interact with lin class and animate the line
+ *
+ * @author  Gabe Bigas
+ * @version 1.0
+ * @since   2021-04-02
+ */
+
+package com.example.demo;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class RRect extends Rectangle {
+
+    Rect r;
+    /**
+     * Constructor of classs
+     * @pararm rect (Rect) is the Rect that you want the shape to get its data from
+     *
+     */
+    public RRect(Rect rect){
+        super();
+
+        r = rect;
+
+        this.setX(r.getX());
+        this.setY(r.getY());
+        this.setWidth(r.getWidth());
+        this.setHeight(r.getLength());
+
+        this.setFill(Color.rgb(r.getColour(0),r.getColour(1),r.getColour(2)));
+        this.setStrokeWidth(r.getBorderThickness());
+        this.setStroke(Color.rgb(r.getBC(0),r.getBC(1),r.getBC(2)));
+    }
+
+    /**
+     * Updates all the attributes based off the effects that occur in the rect class
+     *
+     */
+    public void update(){
+        r.update();
+        this.setX(r.getX());
+        this.setY(r.getY());
+        this.setFill(Color.rgb(r.getColour(0),r.getColour(1),r.getColour(2)));
+
+        if(r.isShown()){
+            this.setOpacity(1);
+        }
+        else{
+            this.setOpacity(0);
+        }
+    }
+}
